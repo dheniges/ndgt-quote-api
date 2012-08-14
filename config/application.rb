4cd38da334df5e0e4b68fa9db50b7af30e3f2bb7
Dirk Heniges <dheniges@gmail.com>
2012-08-14 11:45:05 -0700

    Turning back on asset pipeline, hopefully it works with heroku

--- config/application.rb
+++ config/application.rb
@@ -54,8 +54,7 @@ module Enviroapp
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    # Disabling to test with heroku
-    config.assets.enabled = false
+    config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
